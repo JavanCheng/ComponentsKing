f998593a00998bf9719c975cc88442afb4ed09f9
Chengyf <1351416566@qq.com>
2022-08-06 22:46:26 +0800

    feat: 新增 Menu 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Button from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu>
+          <MenuItem>1</MenuItem>
+          <MenuItem>2</MenuItem>
+        </Menu>
         <Button onClick={(e) => console.log(e.target)}>123</Button>
         <Button btnType="primary">123</Button>
         <Button btnType="primary" size="lg">123</Button>
