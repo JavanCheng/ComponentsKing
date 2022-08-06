f7d79f0356528e3915c4360a42bf515e7ce26668
Chengyf <1351416566@qq.com>
2022-08-06 17:41:47 +0800

    feat: 完成 Button 组件

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button>123</Button>
+        <Button onClick={(e) => console.log(e.target)}>123</Button>
         <Button btnType="primary">123</Button>
         <Button btnType="primary" size="lg">123</Button>
         <Button btnType="primary" size="lg" disabled>123</Button>
+        <Button btnType="danger" size="sm" disabled>123</Button>
+        <Button btnType="link" size="lg" disabled>123</Button>
+        <Button autoFocus>123</Button>
         <h1>12345</h1>
       </header>
     </div>
